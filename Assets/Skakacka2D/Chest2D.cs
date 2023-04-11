5a88f5c234c3baa1353b5f0ee96d546cda23a52f
vratislavino <vratislavino@gmail.com>
2023-04-11 09:26:08 +0200

    ready for coin animation

--- Assets/Skakacka2D/Chest2D.cs
+++ Assets/Skakacka2D/Chest2D.cs
@@ -5,32 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Chest2D : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-
     [SerializeField]
     private string sceneToLoad;
 
     [SerializeField]
     private GameObject canvas;
 
-    [SerializeField]
-    private Sprite openedChest;
-
     private bool isPlayerPresent = false;
 
-
-
-    [SerializeField]
-    private Sprite closedChest;
+    private Animator chestAnimator;
 
     private void Start() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        chestAnimator = GetComponentInChildren<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
         var player = col.GetComponentInParent<PlayerMovement2D>();
         if(player) {
-            spriteRenderer.sprite = openedChest;
+            chestAnimator.SetBool("Opening", true);
             isPlayerPresent = true;
             canvas.SetActive(true);
         }
@@ -39,7 +31,7 @@ public class Chest2D : MonoBehaviour
     private void OnTriggerExit2D(Collider2D col) {
         var player = col.GetComponentInParent<PlayerMovement2D>();
         if (player) {
-            spriteRenderer.sprite = closedChest;
+            chestAnimator.SetBool("Opening", false);
             isPlayerPresent = false;
             canvas.SetActive(false);
         }
@@ -47,7 +39,9 @@ public class Chest2D : MonoBehaviour
 
     private void Update() {
         if (isPlayerPresent && Input.GetKeyDown(KeyCode.E)) {
-            SceneManager.LoadScene(sceneToLoad);
+            //SceneManager.LoadScene(sceneToLoad);
+            chestAnimator.SetTrigger("Destroy");
+            Destroy(gameObject, 1f);
         }
     }
 }
