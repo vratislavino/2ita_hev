5bead1c54485e8ac1e02431aa0abce1148657b34
vratislavino <vratislavino@gmail.com>
2023-04-17 08:07:26 +0200

    :)

--- Assets/Skakacka2D/Coin2D.cs
+++ Assets/Skakacka2D/Coin2D.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Coin2D : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer sr;
+    //[SerializeField] private SpriteRenderer sr;
+
+    Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D col) {
         var player = col.GetComponentInParent<PlayerMovement2D>();
         if(player != null) {
-            sr.color = GetComponent<SpriteRenderer>().color;
-            //Destroy(gameObject);
+            //sr.color = GetComponent<SpriteRenderer>().color;
+            //
+            Debug.Log("asasd");
+            animator.SetTrigger("Destroy");
+            Destroy(gameObject,1f);
         }
     }
 }
